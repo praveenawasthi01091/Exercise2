1b3019abed1a119525ae2ec0c36886b7821aaafe
praveenawasthi01091 <praveenawasthi01091@gmail.com>
2019-10-09 02:23:19 +0530

    "UserName commit 2"

--- src/main/java/com/stackroute/controller/UserController.java
+++ src/main/java/com/stackroute/controller/UserController.java
@@ -1,12 +1,10 @@
-package com.stackroute;
+package com.stackroute.controller;
 
+import com.stackroute.module.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.support.HttpRequestHandlerServlet;
 import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController
--- src/main/java/com/stackroute/module/User.java
+++ src/main/java/com/stackroute/module/User.java
@@ -1,4 +1,4 @@
-package com.stackroute;
+package com.stackroute.module;
 
 public class User {
     private String name;
