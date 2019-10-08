5addfbaddbd836eab9fe2173105a9f5d133c81a4
praveenawasthi01091 <praveenawasthi01091@gmail.com>
2019-10-09 02:32:16 +0530

    "UserName and  Password commit 2"

--- src/main/java/com/stackroute/controller/UserController.java
+++ src/main/java/com/stackroute/controller/UserController.java
@@ -1,17 +1,21 @@
-package com.stackroute;
+package com.stackroute.controller;
 
+import com.stackroute.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.support.HttpRequestHandlerServlet;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController
 {
+    @RequestMapping(value = "/")
+    public String mymethod()
+    {
+        return "index";
+    }
+
     @RequestMapping(value = "/show")
     public ModelAndView getUserName(HttpServletRequest httpServletRequest)
     {
@@ -20,7 +24,7 @@ public class UserController
         User user1=  new User();
         user1.setName(username);
         user1.setPassword(password);
-        ModelAndView modelAndView = new ModelAndView("display.jsp");
+        ModelAndView modelAndView = new ModelAndView("display");
         modelAndView.addObject("user",user1);
         return modelAndView;
     }
--- src/main/java/com/stackroute/model/User.java
+++ src/main/java/com/stackroute/model/User.java
@@ -1,4 +1,4 @@
-package com.stackroute;
+package com.stackroute.model;
 
 public class User {
     private String name;
