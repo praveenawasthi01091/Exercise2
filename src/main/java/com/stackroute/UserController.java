6447d8a9041a486786c378ff700329915698cffd
praveenawasthi01091 <praveenawasthi01091@gmail.com>
2019-10-09 02:16:27 +0530

    "UserName commit"

--- src/main/java/com/stackroute/UserController.java
+++ src/main/java/com/stackroute/UserController.java
@@ -5,22 +5,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.support.HttpRequestHandlerServlet;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController
 {
-    @RequestMapping(value = "/show")
+    @RequestMapping(value = "/")
     public ModelAndView getUserName(HttpServletRequest httpServletRequest)
     {
-        String username=(httpServletRequest.getParameter("username"));
-        String password=(httpServletRequest.getParameter("password"));
+        System.out.println("Inside method");
         User user1=  new User();
-        user1.setName(username);
-        user1.setPassword(password);
-        ModelAndView modelAndView = new ModelAndView("display.jsp");
+        user1.setName("Praveen");
+        user1.setPassword("123");
+        ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("user",user1);
         return modelAndView;
     }
